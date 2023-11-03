a0cbcac312091e809eb5a12b227e14d0e70a6961
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-11-03 13:17:41 -0500

    Working on getting preprocessing working.

--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -44,14 +44,19 @@ class TestMetadataPreprocesser(unittest.TestCase):
 
     def setUp(self):
 
-        self.transformer = preprocess.MetadataPreprocesser()
+        self.expected_cols = ['filepaths', 'sensor_x', 'sensor_y']
+        self.transformer = preprocess.MetadataPreprocesser(
+            output_columns=self.expected_cols
+        )
         image_dir = './test/test_data/images'
         self.fps = preprocess.discover_data(image_dir)
+        self.n_files = len(self.fps)
+        self.fps = np.array([self.fps,])
 
     def test_output(self):
         '''The output prior to any form of georeferencing.
         '''
 
         output_df = self.transformer.fit_transform(self.fps)
-        expected_cols = ['filepaths', 'sensor_x', 'sensor_y']
-        assert (~output_df.columns.isin(expected_cols)).sum() == 0
+        assert len(output_df) == self.n_files
+        assert (~output_df.columns.isin(self.expected_cols)).sum() == 0
