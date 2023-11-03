5409c7673ef6d0b1f074772fe34f4aa64b54e860
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-11-03 12:22:32 -0500

    Added another discover_data test.

--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -24,6 +24,21 @@ class TestDiscoverData(unittest.TestCase):
         fps = preprocess.discover_data(image_dir)
         assert fps == expected_fps
 
+    def test_discover_data_exts(self):
+
+        image_dir = './test/test_data'
+        expected_fps = [
+            ('./test/test_data/images/23085686/'
+             '20220413_221313_1020286912_0_50_3.raw'),
+            ('./test/test_data/images/23085687/'
+             '20220413_202740_745696_1_50_0.raw'),
+            './test/test_data/referenced_images/Geo 225856_1473511261_0.tif',
+            './test/test_data/referenced_images/Geo 836109848_1.tif',
+        ]
+
+        fps = preprocess.discover_data(image_dir, ['raw', 'tif*'])
+        assert fps == expected_fps
+
 
 class TestMetadataPreprocesser(unittest.TestCase):
 
