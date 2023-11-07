9ebc75664343ab37f415d6bf1a5af9a5f34b497c
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-11-07 13:14:41 -0600

    Working on setting up the referenced mosaic.
    Former-commit-id: e8f92ff1f5e94f394ef529e5059f58dfc2239caa

--- test/test_preprocess.py
+++ test/test_preprocess.py
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 
 import night_horizons.preprocess as preprocess
+import night_horizons.utils as utils
 
 
 class TestDiscoverData(unittest.TestCase):
@@ -136,3 +137,33 @@ class TestGeoTIFFPreprocesser(unittest.TestCase):
         # Ensure the conversion was done, i.e. there shouldn't really be
         # values to zero, unless our test dataset was within 1000m of (0,0)
         assert np.nanmax(np.abs(X['x_min'])) > 1000
+
+
+class TestGeoBoundsPreprocesser(unittest.TestCase):
+
+    def test_output(self):
+
+        rng = np.random.default_rng()
+
+        # Image filetree info
+        n_files = 10
+        X_in = pd.DataFrame(
+            data={
+                'x_min': rng.normal(size=n_files),
+                'width': rng.normal(size=n_files),
+                'x_rot': rng.normal(size=n_files),
+                'y_max': rng.normal(size=n_files),
+                'height': rng.normal(size=n_files),
+                'y_rot': rng.normal(size=n_files),
+                'n_x': rng.randint(100, 1000, size=n_files),
+                'n_y': rng.randint(100, 1000, size=n_files),
+            }
+        )
+
+        transformer = preprocess.GeoBoundsPreprocesser()
+        X = transformer.fit_transform(X_in)
+
+        utils.check_columns(
+            X.columns,
+            preprocess.GEOBOUNDS_COLS,
+        )
--- test/test_sklearn.py
+++ test/test_sklearn.py
@@ -1,11 +1,12 @@
 import pytest
 from sklearn.utils.estimator_checks import check_estimator
 
-from night_horizons import reference
+from night_horizons import preprocess
+
 
 @pytest.mark.parametrize(
     "estimator",
-    [reference.SensorGeoreferencer(),]
+    [preprocess.NITELitePreprocesser(),]
 )
 def test_all_estimators(estimator):
     return check_estimator(estimator)
