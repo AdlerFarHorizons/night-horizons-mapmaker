c907c91a757bc329081af4491ceb1e72574f0e06
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-11-08 18:59:57 -0600

    image_to_image_ccoeff now only compares non-zero values by default.

--- night_horizons/metrics.py
+++ night_horizons/metrics.py
@@ -8,6 +8,7 @@ def image_to_image_ccoeff(
     src_img: np.ndarray,
     dst_img: np.ndarray,
     allow_resize: bool = True,
+    compare_non_empty: bool = True,
     tm_metric=cv2.TM_CCOEFF_NORMED
 ):
 
@@ -15,6 +16,13 @@ def image_to_image_ccoeff(
         if not allow_resize:
             raise ValueError('Images must have the same shape.')
         src_img = cv2.resize(src_img, (dst_img.shape[1], dst_img.shape[0]))
+
+    if compare_non_empty:
+        empty_src = np.isclose(src_img.sum(axis=2), 0.)
+        empty_dst = np.isclose(dst_img.sum(axis=2), 0.)
+        either_empty = np.logical_or(empty_src, empty_dst)
+        src_img[either_empty] = 0
+        dst_img[either_empty] = 0
 
     r = cv2.matchTemplate(src_img, dst_img, tm_metric)[0][0]
 
