0e2b8dd3fb6f1387e6239e4d56e1ee70a45aa4cb
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2023-11-16 11:15:18 -0600

    Fixed bug in discover_data tests.

--- test/test_utils.py
+++ test/test_utils.py
@@ -34,16 +34,18 @@ class TestDiscoverData(unittest.TestCase):
             ('./test/test_data/images/23085687/'
              '20220413_202740_745696_1_50_0.raw'),
             './test/test_data/referenced_images/Geo 225856_1473511261_0.tif',
+            './test/test_data/referenced_images/Geo 843083290_1.tif',
             './test/test_data/referenced_images/Geo 836109848_1.tif',
         ]
 
         fps = utils.discover_data(image_dir, ['raw', 'tif', 'tiff'])
         assert list(fps) == expected_fps
 
-    def test_discover_data_patter(self):
+    def test_discover_data_pattern(self):
 
         image_dir = './test/test_data'
         expected_fps = [
+            './test/test_data/referenced_images/Geo 843083290_1.tif',
             './test/test_data/referenced_images/Geo 836109848_1.tif',
         ]
 
