3ecca003bb3695e9f8de49ebcb0ed277997d9338
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2024-03-03 00:47:12 +0000

    Changed to use environment variable.

--- test/test_db.py
+++ test/test_db.py
@@ -1,17 +1,13 @@
+import os
 import unittest
 import psycopg2
 
 
 class TestDatabaseConnection(unittest.TestCase):
     def test_sql_connection(self):
+        url = os.getenv('DATABASE_URL')
         try:
-            conn = psycopg2.connect(
-                host="database",
-                port=5432,
-                user="nitelite",
-                password="nitelite",
-                database="nitelite"
-            )
+            conn = psycopg2.connect(url)
             self.assertTrue(conn)
         except psycopg2.OperationalError:
             self.fail("Failed to establish SQL connection")
